ee8546dce50e7f5d64168bcab5af711e9755c78e
Alexander Pirnarev <sashopirnarev@gmail.com>
2016-01-07 20:59:26 +0200

    added few methods

--- panda.rb
+++ panda.rb
@@ -1,4 +1,27 @@
 class Panda
 
-  def initialize(name,)
+  attr_accessor :name, :email, :gender
+
+  def initialize(name, email, gender)
+    @name, @email, @gender = name, email, gender
+  end
+
+  def male?
+    gender == "male"
+  end
+
+  def female?
+    gender == "female"
+  end
+
+  def to_s
+    "Name: #{name}, email: #{email}, gender: #{gender}"
+  end
+
+  def to_hash
+  end
+
+  def equal?(other)
+    name == other.name && email == other.email && gender == other.gender
+  end
 end
