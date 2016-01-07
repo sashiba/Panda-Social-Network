ba88c41de901879d249e4c94efb117c115589f72
Mila Ruseva <msruseva@gmail.com>
2016-01-07 21:25:40 +0200

    Creat panda and some more

--- panda.rb
+++ panda.rb
@@ -8,7 +8,49 @@ class Panda
     @gender = gender
   end
 
+  def male?
+    true if gender == 'male'
+  end
 
+  def female?
+    true if gender == 'female'
+  end
 
+  def to_s
+    "Name: #{name}, Email: #{email}, Gender: #{gender}"
+  end
+
+  def to_h
+
+  end
+
+  def equal?(other)
+    name == other.name and email == other.email and gender == other.gender
+  end
 
 end
+
+class PandaSocialNetwork
+
+  attr_accessor :network
+
+  def initialize
+    @network = []
+  end
+
+  def add_panda(panda)
+    raise 'PandaAlreadyThere' if network.include? panda
+    network << panda
+  end
+
+  def has_panda(panda)
+  end
+end
+
+network = PandaSocialNetwork.new
+ivo = Panda.new("Ivo", "ivo@pandamail.com", "male")
+rado = Panda.new("Rado", "rado@pandamail.com", "male")
+
+
+network.add_panda(ivo)
+network.add_panda(rado)
