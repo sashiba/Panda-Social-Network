81c911a6ee3fdeb90a57990af4724638c1174fb9
Alexander Pirnarev <sashopirnarev@gmail.com>
2016-01-16 17:01:58 +0200

    added module PandaSocialNetwork

--- panda.rb
+++ panda.rb
@@ -1,148 +1,186 @@
+module PandaSocialNetwork
+  require 'json'
+  require 'yaml'
 
-class Panda
-  attr_accessor :name, :email, :gender, :friends
+  class Panda
+    attr_accessor :name, :email, :gender, :friends
 
-  def initialize(name, email, gender)
-    @name = name
-    @email = email
-    @gender = gender
-    @friends = []
-  end
-
-  def male?
-    gender == 'male'
-  end
-
-  def female?
-    gender == 'female'
-  end
-
-  def to_s
-    "Name: #{name}, Email: #{email}, Gender: #{gender}"
-  end
-
-  def to_proc
-    lambda do |x, *args|
-      x.public_send self, *args
+    def initialize(name, email, gender)
+      @name = name
+      @email = email
+      @gender = gender
+      @friends = []
     end
-  end
 
-  def ==(other)
-    name == other.name and email == other.email and gender == other.gender
-  end
+    def male?
+      gender == 'male'
+    end
 
-  def hash
-    to_s.hash
-  end
+    def female?
+      gender == 'female'
+    end
 
-  alias_method :eql?, :==
-end
+    def to_s
+      "Name: #{name}, Email: #{email}, Gender: #{gender}"
+    end
 
-class PandaSocialNetwork
-  attr_accessor :network
-
-  def initialize
-    @network = {}
-  end
-
-  def add_panda(panda)
-    raise "PandaAlreadyThere" if has_panda(panda)
-
-    network[panda] = panda.friends
-  end
-
-  def has_panda(panda)
-    return true if network.key? panda
-
-    false
-  end
-
-  def make_friends(panda1, panda2)
-    raise "PandasAlreadyFriends" if are_friends(panda1, panda2)
-
-    add_panda(panda1) unless has_panda(panda1)
-    add_panda(panda2) unless has_panda(panda2)
-
-    panda2.friends << panda1 unless panda2.friends.include? panda1
-    panda1.friends << panda2 unless panda1.friends.include? panda2
-  end
-
-  def are_friends(panda1, panda2)
-    panda1.friends.include?(panda2) && panda2.friends.include?(panda1)
-  end
-
-  def friends_of(panda)
-    return false unless has_panda(panda)
-
-    panda.friends
-  end
-
-  def connection_level(panda1, panda2)
-    return false unless has_panda(panda1) and has_panda(panda2)
-    levels = bfs_1(panda1, panda2)
-
-    levels
-  end
-
-  def bfs_1(start_panda, wanted_panda)
-    q = Queue.new
-    visited = []
-    q << [0, start_panda]
-    visited << start_panda
-
-    until q.empty?
-      level, current = q.pop
-      return level if wanted_panda == current
-
-      un = network[current].select { |v| !visited.include? v }
-      un.each do |v|
-        visited << v
-        q << [level + 1, v]
+    def to_proc
+      lambda do |x, *args|
+        x.public_send self, *args
       end
     end
 
-    -1
-  end
-
-  def are_connected(panda1, panda2)
-    return false unless connection_level(panda1, panda2)
-
-    connection_level(panda1, panda2) == -1 ? false : true
-  end
-
-  def bfs_gender_on_level(start_panda, search_level, gender)
-    q = Queue.new
-    visited, result = [], 0
-    q << [0, start_panda]
-    visited << start_panda
-
-    until q.empty?
-      level, current = q.pop
-      result += 1 if level == search_level and gender == current.gender
-
-      un = network[current].select { |v| !visited.include? v }
-      un.each do |v|
-        visited << v
-        q << [level + 1, v]
-      end
+    def ==(other)
+      name == other.name && email == other.email && gender == other.gender
     end
 
-    result
+    def hash
+      to_s.hash
+    end
+
+    def to_yaml
+      YAML.dump({ name: @name,
+                  email: @email,
+                  gender: @gender,
+                  friends: @friends
+                })
+    end
+
+    def self.from_yaml(string)
+      data = YAML.load string
+      self.new(data[:name], data[:email], data[:gender])
+      #self.friends = data[:friends]
+    end
+
+    alias_method :eql?, :==
   end
 
-  def how_many_gender_in_network(level, panda, gender)
-    bfs_gender_on_level(panda, level, gender)
-  end
+  class PandaSocialNetwork
+    attr_accessor :network
 
-  def size
-    network.size
-  end
+    def initialize
+      @network = {}
+    end
 
-  def save(file_name)
-    File.open("#{file_name}.psn", 'w+') { |f| Marshal.dump(self, f) }
-  end
+    def add_panda(panda)
+      raise "PandaAlreadyThere" if has_panda(panda)
 
-  def load(file_name)
-    File.open("#{file_name}.psn", 'r') { |f| Marshal.load(f) }
+      network[panda] = panda.friends
+    end
+
+    def has_panda(panda)
+      return true if network.key? panda
+
+      false
+    end
+
+    def make_friends(panda1, panda2)
+      raise "PandasAlreadyFriends" if are_friends(panda1, panda2)
+
+      add_panda(panda1) unless has_panda(panda1)
+      add_panda(panda2) unless has_panda(panda2)
+
+      panda2.friends << panda1 unless panda2.friends.include? panda1
+      panda1.friends << panda2 unless panda1.friends.include? panda2
+    end
+
+    def are_friends(panda1, panda2)
+      panda1.friends.include?(panda2) && panda2.friends.include?(panda1)
+    end
+
+    def friends_of(panda)
+      return false unless has_panda(panda)
+
+      panda.friends
+    end
+
+    def connection_level(panda1, panda2)
+      return false unless has_panda(panda1) && has_panda(panda2)
+      levels = bfs_1(panda1, panda2)
+
+      levels
+    end
+
+    def bfs_1(start_panda, wanted_panda)
+      q = Queue.new
+      visited = []
+      q << [0, start_panda]
+      visited << start_panda
+
+      until q.empty?
+        level, current = q.pop
+        return level if wanted_panda == current
+
+        un = network[current].select { |v| !visited.include? v }
+        un.each do |v|
+          visited << v
+          q << [level + 1, v]
+        end
+      end
+
+      -1
+    end
+
+    def are_connected(panda1, panda2)
+      return false unless connection_level(panda1, panda2)
+
+      connection_level(panda1, panda2) == -1 ? false : true
+    end
+
+    def bfs_gender_on_level(start_panda, search_level, gender)
+      q = Queue.new
+      visited, result = [], 0
+      q << [0, start_panda]
+      visited << start_panda
+
+      until q.empty?
+        level, current = q.pop
+        result += 1 if level == search_level && gender == current.gender
+
+        un = network[current].select { |v| !visited.include? v }
+        un.each do |v|
+          visited << v
+          q << [level + 1, v]
+        end
+      end
+
+      result
+    end
+
+    def how_many_gender_in_network(level, panda, gender)
+      bfs_gender_on_level(panda, level, gender)
+    end
+
+    def size
+      network.size
+    end
+
+    def save(file_name)
+      File.open("#{file_name}.psn", 'w+') { |f| Marshal.dump(self, f) }
+    end
+
+    def load(file_name)
+      File.open("#{file_name}.psn", 'r') { |f| Marshal.load(f) }
+    end
+
+    def to_json
+      { 'network' => @network.each { |panda| panda } }
+    end
+
+    def json_create(network_json)
+      @network = network_json['network']
+    end
+
+    def save_json(file_name)
+      # PandaSocialNetwork.new
+      # a.json_create network.to_json
+    end
+
+    def to_yaml
+      YAML.dump network.each(&:to_yaml)
+    end
+
+    def self.from_yaml(string)
+    end
   end
 end
--- panda_test.rb
+++ panda_test.rb
@@ -3,39 +3,39 @@ require 'minitest/autorun'
 require_relative 'panda.rb'
 
 class PandaClass < Minitest::Test
-  def  test_panda_name
-    ivo = Panda.new("Ivo", "ivo@pandamail.com", "male")
+  def test_panda_name
+    ivo = PandaSocialNetwork::Panda.new('Ivo', 'ivo@pandamail.com', 'male')
 
-    assert_equal "Ivo", ivo.name
+    assert_equal 'Ivo', ivo.name
   end
 
-  def  test_panda_email
-    ivo = Panda.new("Ivo", "ivo@pandamail.com", "male")
+  def test_panda_email
+    ivo = PandaSocialNetwork::Panda.new('Ivo', 'ivo@pandamail.com', 'male')
 
-    assert_equal "ivo@pandamail.com", ivo.email
+    assert_equal 'ivo@pandamail.com', ivo.email
   end
 
-  def  test_panda_gender
-    ivo = Panda.new("Ivo", "ivo@pandamail.com", "male")
+  def test_panda_gender
+    ivo = PandaSocialNetwork::Panda.new('Ivo', 'ivo@pandamail.com', 'male')
 
-    assert_equal "male", ivo.gender
+    assert_equal 'male', ivo.gender
   end
 
-  def  test_panda_gender_male?
-    ivo = Panda.new("Ivo", "ivo@pandamail.com", "male")
+  def test_panda_gender_male?
+    ivo = PandaSocialNetwork::Panda.new('Ivo', 'ivo@pandamail.com', 'male')
 
     assert_equal true, ivo.male?
   end
 
-  def  test_panda_gender_female?
-    ivo = Panda.new("Ivo", "ivo@pandamail.com", "male")
+  def test_panda_gender_female?
+    ivo = PandaSocialNetwork::Panda.new('Ivo', 'ivo@pandamail.com', 'male')
 
     assert_equal false, ivo.female?
   end
 
-  def  test_panda_to_s
-    ivo = Panda.new("Ivo", "ivo@pandamail.com", "male")
-    expected = "Name: Ivo, Email: ivo@pandamail.com, Gender: male"
+  def test_panda_to_s
+    ivo = PandaSocialNetwork::Panda.new('Ivo', 'ivo@pandamail.com', 'male')
+    expected = 'Name: Ivo, Email: ivo@pandamail.com, Gender: male'
 
     assert_equal expected, ivo.to_s
   end
@@ -43,10 +43,10 @@ end
 
 class SocialNetworkClass < Minitest::Test
   def test_add_panda_to_the_network
-    network = PandaSocialNetwork.new
-    ivo = Panda.new("Ivo", "ivo@pandamail.com", "male")
-    rado = Panda.new("Rado", "rado@pandamail.com", "male")
-    tony = Panda.new("Tony", "tony@pandamail.com", "female")
+    network = PandaSocialNetwork::PandaSocialNetwork.new
+    ivo = PandaSocialNetwork::Panda.new('Ivo', 'ivo@pandamail.com', 'male')
+    rado = PandaSocialNetwork::Panda.new('Rado', 'rado@pandamail.com', 'male')
+    tony = PandaSocialNetwork::Panda.new('Tony', 'tony@pandamail.com', 'female')
 
     network.add_panda(ivo)
     network.add_panda(rado)
@@ -56,26 +56,26 @@ class SocialNetworkClass < Minitest::Test
   end
 
   def test_has_panda_method_with_existing_panda
-    network = PandaSocialNetwork.new
-    ivo = Panda.new("Ivo", "ivo@pandamail.com", "male")
+    network = PandaSocialNetwork::PandaSocialNetwork.new
+    ivo = PandaSocialNetwork::Panda.new('Ivo', 'ivo@pandamail.com', 'male')
     network.add_panda(ivo)
 
     assert_equal true, network.has_panda(ivo)
   end
 
   def test_has_panda_with_nonexistent_panda
-    network = PandaSocialNetwork.new
-    ivo = Panda.new("Ivo", "ivo@pandamail.com", "male")
-    rado = Panda.new("Rado", "rado@pandamail.com", "male")
+    network = PandaSocialNetwork::PandaSocialNetwork.new
+    ivo = PandaSocialNetwork::Panda.new('Ivo', 'ivo@pandamail.com', 'male')
+    rado = PandaSocialNetwork::Panda.new('Rado', 'rado@pandamail.com', 'male')
     network.add_panda(ivo)
 
     assert_equal false, network.has_panda(rado)
   end
 
   def test_make_panda_friends
-    network = PandaSocialNetwork.new
-    ivo = Panda.new("Ivo", "ivo@pandamail.com", "male")
-    rado = Panda.new("Rado", "rado@pandamail.com", "male")
+    network = PandaSocialNetwork::PandaSocialNetwork.new
+    ivo = PandaSocialNetwork::Panda.new('Ivo', 'ivo@pandamail.com', 'male')
+    rado = PandaSocialNetwork::Panda.new('Rado', 'rado@pandamail.com', 'male')
     network.add_panda(ivo)
     network.add_panda(rado)
     network.make_friends(ivo, rado)
@@ -84,9 +84,9 @@ class SocialNetworkClass < Minitest::Test
   end
 
   def test__connection_level_1
-    network = PandaSocialNetwork.new
-    ivo = Panda.new("Ivo", "ivo@pandamail.com", "male")
-    rado = Panda.new("Rado", "rado@pandamail.com", "male")
+    network = PandaSocialNetwork::PandaSocialNetwork.new
+    ivo = PandaSocialNetwork::Panda.new('Ivo', 'ivo@pandamail.com', 'male')
+    rado = PandaSocialNetwork::Panda.new('Rado', 'rado@pandamail.com', 'male')
     network.add_panda(ivo)
     network.add_panda(rado)
 
@@ -97,10 +97,10 @@ class SocialNetworkClass < Minitest::Test
   end
 
   def test__connection_level_2
-    network = PandaSocialNetwork.new
-    ivo = Panda.new("Ivo", "ivo@pandamail.com", "male")
-    rado = Panda.new("Rado", "rado@pandamail.com", "male")
-    tony = Panda.new("Tony", "tony@gmail.com", "female")
+    network = PandaSocialNetwork::PandaSocialNetwork.new
+    ivo = PandaSocialNetwork::Panda.new('Ivo', 'ivo@pandamail.com', 'male')
+    rado = PandaSocialNetwork::Panda.new('Rado', 'rado@pandamail.com', 'male')
+    tony = PandaSocialNetwork::Panda.new('Tony', 'tony@gmail.com', 'female')
     network.add_panda(ivo)
     network.add_panda(rado)
     network.add_panda(tony)
@@ -112,10 +112,10 @@ class SocialNetworkClass < Minitest::Test
   end
 
   def test__connection_level_no_connection
-    network = PandaSocialNetwork.new
-    ivo = Panda.new("Ivo", "ivo@pandamail.com", "male")
-    rado = Panda.new("Rado", "rado@pandamail.com", "male")
-    tony = Panda.new("Tony", "tony@gmail.com", "female")
+    network = PandaSocialNetwork::PandaSocialNetwork.new
+    ivo = PandaSocialNetwork::Panda.new('Ivo', 'ivo@pandamail.com', 'male')
+    rado = PandaSocialNetwork::Panda.new('Rado', 'rado@pandamail.com', 'male')
+    tony = PandaSocialNetwork::Panda.new('Tony', 'tony@gmail.com', 'female')
     network.add_panda(ivo)
     network.add_panda(rado)
     network.add_panda(tony)
@@ -126,18 +126,18 @@ class SocialNetworkClass < Minitest::Test
   end
 
   def test__connection_level_with_a_panda_not_in_network
-    network = PandaSocialNetwork.new
-    ivo = Panda.new("Ivo", "ivo@pandamail.com", "male")
-    rado = Panda.new("Rado", "rado@pandamail.com", "male")
+    network = PandaSocialNetwork::PandaSocialNetwork.new
+    ivo = PandaSocialNetwork::Panda.new('Ivo', 'ivo@pandamail.com', 'male')
+    rado = PandaSocialNetwork::Panda.new('Rado', 'rado@pandamail.com', 'male')
     network.add_panda(ivo)
 
     assert_equal false, network.connection_level(ivo, rado)
   end
 
   def test_are_friends_method
-    network = PandaSocialNetwork.new
-    ivo = Panda.new("Ivo", "ivo@pandamail.com", "male")
-    rado = Panda.new("Rado", "rado@pandamail.com", "male")
+    network = PandaSocialNetwork::PandaSocialNetwork.new
+    ivo = PandaSocialNetwork::Panda.new('Ivo', 'ivo@pandamail.com', 'male')
+    rado = PandaSocialNetwork::Panda.new('Rado', 'rado@pandamail.com', 'male')
     network.add_panda(ivo)
     network.add_panda(rado)
     network.make_friends(ivo, rado)
@@ -146,9 +146,9 @@ class SocialNetworkClass < Minitest::Test
   end
 
   def test_are__not_friends_method
-    network = PandaSocialNetwork.new
-    ivo = Panda.new("Ivo", "ivo@pandamail.com", "male")
-    rado = Panda.new("Rado", "rado@pandamail.com", "male")
+    network = PandaSocialNetwork::PandaSocialNetwork.new
+    ivo = PandaSocialNetwork::Panda.new('Ivo', 'ivo@pandamail.com', 'male')
+    rado = PandaSocialNetwork::Panda.new('Rado', 'rado@pandamail.com', 'male')
     network.add_panda(ivo)
     network.add_panda(rado)
 
@@ -156,9 +156,9 @@ class SocialNetworkClass < Minitest::Test
   end
 
   def test_are_connected
-    network = PandaSocialNetwork.new
-    ivo = Panda.new("Ivo", "ivo@pandamail.com", "male")
-    rado = Panda.new("Rado", "rado@pandamail.com", "male")
+    network = PandaSocialNetwork::PandaSocialNetwork.new
+    ivo = PandaSocialNetwork::Panda.new('Ivo', 'ivo@pandamail.com', 'male')
+    rado = PandaSocialNetwork::Panda.new('Rado', 'rado@pandamail.com', 'male')
     network.add_panda(ivo)
     network.add_panda(rado)
     network.make_friends(ivo, rado)
@@ -167,10 +167,10 @@ class SocialNetworkClass < Minitest::Test
   end
 
   def test_are_connected_between_friends
-    network = PandaSocialNetwork.new
-    ivo = Panda.new("Ivo", "ivo@pandamail.com", "male")
-    rado = Panda.new("Rado", "rado@pandamail.com", "male")
-    tony = Panda.new("Tony", "tony@gmail.com", "female")
+    network = PandaSocialNetwork::PandaSocialNetwork.new
+    ivo = PandaSocialNetwork::Panda.new('Ivo', 'ivo@pandamail.com', 'male')
+    rado = PandaSocialNetwork::Panda.new('Rado', 'rado@pandamail.com', 'male')
+    tony = PandaSocialNetwork::Panda.new('Tony', 'tony@gmail.com', 'female')
 
     network.add_panda(ivo)
     network.add_panda(rado)
@@ -183,9 +183,9 @@ class SocialNetworkClass < Minitest::Test
   end
 
   def test_are_not_connected
-    network = PandaSocialNetwork.new
-    ivo = Panda.new("Ivo", "ivo@pandamail.com", "male")
-    rado = Panda.new("Rado", "rado@pandamail.com", "male")
+    network = PandaSocialNetwork::PandaSocialNetwork.new
+    ivo = PandaSocialNetwork::Panda.new('Ivo', 'ivo@pandamail.com', 'male')
+    rado = PandaSocialNetwork::Panda.new('Rado', 'rado@pandamail.com', 'male')
     network.add_panda(ivo)
     network.add_panda(rado)
 
@@ -193,33 +193,33 @@ class SocialNetworkClass < Minitest::Test
   end
 
   def test_friends_of_panda_not_from_network
-    network = PandaSocialNetwork.new
-    ivo = Panda.new("Ivo", "ivo@pandamail.com", "male")
-    rado = Panda.new("Rado", "rado@pandamail.com", "male")
+    network = PandaSocialNetwork::PandaSocialNetwork.new
+    ivo = PandaSocialNetwork::Panda.new('Ivo', 'ivo@pandamail.com', 'male')
+    rado = PandaSocialNetwork::Panda.new('Rado', 'rado@pandamail.com', 'male')
     network.add_panda(rado)
 
     assert_equal false, network.friends_of(ivo)
   end
 
   def test_friends_of_panda_in_network
-    network = PandaSocialNetwork.new
-    ivo = Panda.new("Ivo", "ivo@pandamail.com", "male")
-    rado = Panda.new("Rado", "rado@pandamail.com", "male")
+    network = PandaSocialNetwork::PandaSocialNetwork.new
+    ivo = PandaSocialNetwork::Panda.new('Ivo', 'ivo@pandamail.com', 'male')
+    rado = PandaSocialNetwork::Panda.new('Rado', 'rado@pandamail.com', 'male')
     network.add_panda(rado)
     network.add_panda(ivo)
     network.make_friends(ivo, rado)
 
-    expected = "Name: Rado, Email: rado@pandamail.com, Gender: male"
+    expected = 'Name: Rado, Email: rado@pandamail.com, Gender: male'
 
     assert_equal expected, network.friends_of(ivo)[0].to_s
   end
 
   def test_how_many_gender_in_network
-    network = PandaSocialNetwork.new
-    ivo = Panda.new("Ivo", "ivo@pandamail.com", "male")
-    rado = Panda.new("Rado", "rado@pandamail.com", "male")
-    tony = Panda.new("Tony", "tony@gmail.com", "female")
-    maria = Panda.new("Maria", "mmm@gmail.com", "female")
+    network = PandaSocialNetwork::PandaSocialNetwork.new
+    ivo = PandaSocialNetwork::Panda.new('Ivo', 'ivo@pandamail.com', 'male')
+    rado = PandaSocialNetwork::Panda.new('Rado', 'rado@pandamail.com', 'male')
+    tony = PandaSocialNetwork::Panda.new('Tony', 'tony@gmail.com', 'female')
+    maria = PandaSocialNetwork::Panda.new('Maria', 'mmm@gmail.com', 'female')
 
     network.add_panda(ivo)
     network.add_panda(rado)
@@ -230,15 +230,15 @@ class SocialNetworkClass < Minitest::Test
     network.make_friends(rado, tony)
     network.make_friends(rado, maria)
 
-    assert_equal 2, network.how_many_gender_in_network(1, rado, "female")
+    assert_equal 2, network.how_many_gender_in_network(1, rado, 'female')
   end
 
   def test_how_many_gender_in_network_level_2
-    network = PandaSocialNetwork.new
-    ivo = Panda.new("Ivo", "ivo@pandamail.com", "male")
-    rado = Panda.new("Rado", "rado@pandamail.com", "male")
-    tony = Panda.new("Tony", "tony@gmail.com", "female")
-    maria = Panda.new("Maria", "mmm@gmail.com", "female")
+    network = PandaSocialNetwork::PandaSocialNetwork.new
+    ivo = PandaSocialNetwork::Panda.new('Ivo', 'ivo@pandamail.com', 'male')
+    rado = PandaSocialNetwork::Panda.new('Rado', 'rado@pandamail.com', 'male')
+    tony = PandaSocialNetwork::Panda.new('Tony', 'tony@gmail.com', 'female')
+    maria = PandaSocialNetwork::Panda.new('Maria', 'mmm@gmail.com', 'female')
 
     network.add_panda(ivo)
     network.add_panda(rado)
@@ -249,15 +249,15 @@ class SocialNetworkClass < Minitest::Test
     network.make_friends(rado, tony)
     network.make_friends(rado, maria)
 
-    assert_equal 2, network.how_many_gender_in_network(2, ivo, "female")
+    assert_equal 2, network.how_many_gender_in_network(2, ivo, 'female')
   end
 
   def test_how_many_gender_in_network_0_females
-    network = PandaSocialNetwork.new
-    ivo = Panda.new("Ivo", "ivo@pandamail.com", "male")
-    rado = Panda.new("Rado", "rado@pandamail.com", "male")
-    tony = Panda.new("Tony", "tony@gmail.com", "female")
-    maria = Panda.new("Maria", "mmm@gmail.com", "female")
+    network = PandaSocialNetwork::PandaSocialNetwork.new
+    ivo = PandaSocialNetwork::Panda.new('Ivo', 'ivo@pandamail.com', 'male')
+    rado = PandaSocialNetwork::Panda.new('Rado', 'rado@pandamail.com', 'male')
+    tony = PandaSocialNetwork::Panda.new('Tony', 'tony@gmail.com', 'female')
+    maria = PandaSocialNetwork::Panda.new('Maria', 'mmm@gmail.com', 'female')
 
     network.add_panda(ivo)
     network.add_panda(rado)
@@ -268,13 +268,13 @@ class SocialNetworkClass < Minitest::Test
     network.make_friends(rado, tony)
     network.make_friends(rado, maria)
 
-    assert_equal 0, network.how_many_gender_in_network(1, ivo, "female")
+    assert_equal 0, network.how_many_gender_in_network(1, ivo, 'female')
   end
 
   def test_save_network_to_file
-    network = PandaSocialNetwork.new
-    ivo = Panda.new("Ivo", "ivo@pandamail.com", "male")
-    rado = Panda.new("Rado", "rado@pandamail.com", "male")
+    network = PandaSocialNetwork::PandaSocialNetwork.new
+    ivo = PandaSocialNetwork::Panda.new('Ivo', 'ivo@pandamail.com', 'male')
+    rado = PandaSocialNetwork::Panda.new('Rado', 'rado@pandamail.com', 'male')
     network.add_panda(rado)
     network.add_panda(ivo)
     network.make_friends(ivo, rado)
